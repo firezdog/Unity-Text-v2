026a20c8ef734820dee7c671637cfb8561292934
Leibowitz <xaxl172@c02wm82mg8wn.attlocal.net>
2018-08-20 22:03:05 -0500

    added starting text

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] Text textComponent;
+
+    // Use this for initialization
+    void Start()
+    {
+        textComponent.text = 
+            "Welcome to the text adventure." +
+            Environment.NewLine + Environment.NewLine +
+            @"You are sitting in front of a computer, basking in the green glow of the screen. Your belly is still full from supper, and you feel vague and lackadaiscal. Suddenly an idea pops into your head: 'I should program something!' Do you" +
+            Environment.NewLine + Environment.NewLine +
+            "\t(A) fire up Visual Studio Code and begin programming, or" +
+            Environment.NewLine + 
+            "\t(B) get up from the computer and play Persona 5?";
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
